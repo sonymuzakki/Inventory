58300c6373c76fa52170f545e1490ae80d73db57
zaki <nuzakkisony@gmail.com>
2023-02-07 17:09:34 +0700

    landing page

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -55,7 +55,7 @@
                                         </ul>
                                     </div>
                                     <div class="header__btn d-none d-md-block">
-                                        <a href="" class="btn">Logout</a>
+                                        <a href="" class="btn">Log out</a>
                                     </div>
                                 </nav>
                             </div>
@@ -167,6 +167,35 @@
         </main>
         <!-- main-area-end -->
 
+        <section class="homeContact">
+            <div class="container">
+                <div class="homeContact__wrap">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="section__title">
+                                <span class="sub-title">07 - Say hello</span>
+                                <h2 class="title">Any questions? Feel free <br> to contact</h2>
+                            </div>
+                            <div class="homeContact__content">
+                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form</p>
+                                <h2 class="mail"><a href="mailto:Info@webmail.com">Info@webmail.com</a></h2>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="homeContact__form">
+                                <form action="#">
+                                    <input type="text" placeholder="Enter name*">
+                                    <input type="email" placeholder="Enter mail*">
+                                    <input type="number" placeholder="Enter number*">
+                                    <textarea name="message" placeholder="Enter Massage*"></textarea>
+                                    <button type="submit">Send Message</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
         {{--  <!-- Footer-area -->
