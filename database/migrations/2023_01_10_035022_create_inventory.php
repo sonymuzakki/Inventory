04b278bbed67f349851a5abdc6d4cee28c32dda6
zaki <nuzakkisony@gmail.com>
2023-03-16 08:51:07 +0700

    Pagi 16/3

--- database/migrations/2023_01_10_035022_create_inventory.php
+++ database/migrations/2023_01_10_035022_create_inventory.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->integer('jenis_id');
             $table->integer('divisi_id');
             $table->integer('lokasi_id');
-            $table->integer('request_id')->nullable();
             $table->string('hostname')->nullable();
             $table->string('merk')->nullable();
             $table->string('Processor')->nullable();
